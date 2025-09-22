5775d44ae4ac508b73fd0ea93006353e33f65174
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-22 19:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var isDate = function (input) {
-  //   write your code here
+  return !NaN(Date.parse(input))
 };
 
-// Do not change the code below.
+// // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
